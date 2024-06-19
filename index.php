b0938947c742db8bed9e4f5228667ea644e70523
Chastine Evangelista <chastineevangelista@gmail.com>
2024-06-20 07:22:08 +0800

    eto pa lang nagagawa ko, to be distributed yung parts

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $pages = array();
+    $pages["html.php"] = "home";
+    $pages["events.php"] = "events";
+    $pages["news.php"] = "news";
+    $pages["ticketOutlets.php"] = "outlets";
+    $pages["ticketPurchase.php"] = "purchase";
+    $pages["faqSupport.php"] = "faq";
+?>
---
+++ news.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Ez Tickets</title>
+</head>
+<body>
+    <div id="navbar">
+        <div>
+            <div id="logo"></div>
+            <ul>
+                <li><h3>EZ TICKETS</h3></li>
+                <li><a href="home.php" >Home</a></li>
+                <li><a href="events.php">Events</a></li>
+                <li><a href="news.php" class="active">News</a></li>
+                <li><a href="ticketOutlets.php">Ticket Outlets</a></li>
+                <li><a href="ticketPurchase.php">Ticket Purchase</a></li>
+                <li><a href="faqSupport.php">FAQ and Support</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
