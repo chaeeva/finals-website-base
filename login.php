c737f097fb7ddac063b48d4e93339869232f1bed
Chastine Evangelista <chastineevangelista@gmail.com>
2024-06-28 21:45:46 +0800

    added login page, no functionality yet, css not done

--- login.php
+++ login.php
@@ -7,13 +7,38 @@
     <title>Ez Ticketst</title>
 </head>
 <body>
-    <div class="form-outline">
-        <div class="email-username">
-            <form>
-                <label  class="form-label" for="email-username">Email or Username</label>
-                <input type="email" id="email-username" name="email-username" class="form-control">
-            </form>
+<div class="container">
+    <form action="loginIndex.php">
+        <div class="form-outline">
+            <!-- email -->
+            <div class="input-line">
+                <input type="email" name="email-username"id="email-username" class="form-control" placeholder="Email or Username">
+            </div>
+            <br>
+            <!-- password -->
+            <div class="input-line">
+                <input type="password" name="password-login" id="password=login" class="form-control" placeholder="Password">
+            </div>
+            <br>
+<!-- remember me and forgot password -->
+            <div class="row">
+                <div class="column">
+                    <div class="form-check">
+                        <input type="checkbox" name="checkbox" id="checkbox" class="form-check-input" checked>
+                        <label for="checkbox" class="form-check-label">Remember Me</label>
+                    </div>
+                    <div class="column">
+                    &emsp; &emsp; &emsp;<a href="">Forgot Password?</a>
+                    </div>
+                </div>
+            </div>
+
+        
+            <div id="button">
+                <button type="button">Sign In</button>
+            </div>
         </div>
-    </div>
+    </form>
+</div>
 </body>
 </html>
